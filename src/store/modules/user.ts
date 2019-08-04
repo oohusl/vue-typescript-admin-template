8c901fb7d2b66bef2a9d5c60dfe77c5248c62736
oohusl <oohusl@163.com>
2019-08-04 20:26:09 +0800

    remember login

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,66 +1,73 @@
-import { VuexModule, Module, Action, Mutation, getModule } from 'vuex-module-decorators'
-import { login, logout, getUserInfo } from '@/api/users'
-import store from '@/store'
-import { timingSafeEqual } from 'crypto'
-import { rejects } from 'assert'
+import {
+  VuexModule,
+  Module,
+  Action,
+  Mutation,
+  getModule
+} from "vuex-module-decorators";
+import { login, logout, getUserInfo } from "@/api/users";
+import store from "@/store";
+import { timingSafeEqual } from "crypto";
+import { rejects } from "assert";
 
 export interface IUserState {
-  logon: boolean,
-  authenticated: boolean,
-  login?: string,
-  roles? : string[]
+  logon: boolean;
+  authenticated: boolean;
+  login?: string;
+  roles?: string[];
 }
 
-@Module({ dynamic: true, store, name: 'user' })
+@Module({ dynamic: true, store, name: "user" })
 class User extends VuexModule implements IUserState {
-  logon = false
-  authenticated = false
-  login?: string = ''
-  roles?:string[] = []
+  logon = false;
+  authenticated = false;
+  login?: string = "";
+  roles?: string[] = [];
 
   @Mutation
   _authenticated(authenticated: boolean) {
-    this.authenticated = authenticated
+    this.authenticated = authenticated;
     if (!authenticated) {
-      this.login = undefined
-      this.roles = undefined
+      this.login = undefined;
+      this.roles = undefined;
     }
   }
 
   @Mutation
-  _account(account:any) {
-    this.login = account.login
-    this.roles = account.authorities
+  _account(account: any) {
+    this.login = account.login;
+    this.roles = account.authorities;
   }
 
   @Action
-  public async Login(userInfo: { username: string, password: string }) {
-    let { username, password } = userInfo
-    username = username.trim()
-    const { data } = await login({ username, password })
-    const bearerToken = data.id_token
+  public async Login(userInfo: { username: string; password: string }) {
+    let { username, password } = userInfo;
+    username = username.trim();
+    const { data } = await login({ username, password });
+    const bearerToken = data.id_token;
     if (bearerToken) {
       // if (this.rememberMe) {
       //   localStorage.setItem('jhi-authenticationToken', jwt);
       // } else {
-      sessionStorage.setItem('jhi-authenticationToken', bearerToken)
+      localStorage.setItem("jhi-authenticationToken", bearerToken);
       // }
     }
-    this._authenticated(true)
+    this._authenticated(true);
 
     // this.GetUserInfo();
   }
 
   @Action
   public Logout() {
-    this._authenticated(false)
+    this._authenticated(false);
   }
 
   @Action
   public async GetUserInfo() {
-    const { data } = await getUserInfo()
-    this._account(data)
+    const { data } = await getUserInfo();
+    this._authenticated(true);
+    this._account(data);
   }
 }
 
-export const UserModule = getModule(User)
+export const UserModule = getModule(User);
